0425c8cd9f60e3cacfa9ed4548c00403885f843d
JairoKazuo <jkazuodg@gmail.com>
2025-10-01 00:14:37 -0500

    script1

---
+++ PC1/evidencia-hdp/scripts/00_change_passwords.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Para ejecutar manualmente en la VM o WebShell
+echo "Cambiar contraseñas de Linux:"
+echo "  passwd root"
+echo "  passwd maria_dev"
+echo "Cambiar contraseña de Ambari (usuario admin):"
+echo "  ambari-admin-password-reset"
